dc83c58422eba865c63a42f51b2c553473df6a02
Chen Wang <wangcees@gmail.com>
2019-08-22 15:15:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  colors = {}
+  gender = {}
+  lives = {}
+  colors[data.keys[0]] = data.values[0]
+  gender[data.keys[1]] = data.values[1]
+  lives[data.keys[2]] = data.values[2]
+  pigeon_data = {}
+  
+  colors.each do |  |  
+  end
+  pigeon_data
 end
